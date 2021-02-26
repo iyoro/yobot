542b0718959758b61dc510259df2cb0cddc33f16
iyoro <6958668+iyoro@users.noreply.github.com>
2021-02-26 17:50:09 +0000

    Allow command prefix to be parameterised by env

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
     logLevel: process.env.LOG_LEVEL ? process.env.LOG_LEVEL : 'debug',
     clientToken: process.env.CLIENT_TOKEN,
-    commandPrefix: '!',
+    commandPrefix: process.env.COMMAND_PREFIX ? process.env.COMMAND_PREFIX : '!',
 };
