fedd2f44a383389231cdc4e39c023a0dd7cf5317
iyoro <6958668+iyoro@users.noreply.github.com>
2021-10-13 18:05:05 +0000

    Support DM channels

--- spec/util.discord.spec.mjs
+++ spec/util.discord.spec.mjs
@@ -7,15 +7,23 @@ const client = {
 };
 const textChannel = {
     isText() { return true; },
-    isThread() { return false; }
+    isThread() { return false; },
+    type: 'GUILD_TEXT',
 };
 const threadChannel = {
-    isText() { return false; },
-    isThread() { return true; }
+    isText() { return true; },
+    isThread() { return true; },
+    type: 'GUILD_PUBLIC_THREAD',
+};
+const dmChannel = {
+    isText() { return true; },
+    isThread() { return false; },
+    type: 'DM',
 };
 const otherKindOfChannel = {
     isText() { return false; },
-    isThread() { return false; }
+    isThread() { return false; },
+    type: 'UNKNOWN',
 };
 
 describe('Discord utilities', () => {
@@ -40,7 +48,7 @@ describe('Discord utilities', () => {
 
         it('fetches a thread channel by snowflake when allowed', async (done) => {
             const fetch = spyOn(client.channels, 'fetch').withArgs('1').and.resolveTo(threadChannel);
-            getTextChannel(client, '1', true).then(chan => {
+            getTextChannel(client, '1', true, false).then(chan => {
                 expect(chan).toBe(threadChannel);
                 expect(fetch).toHaveBeenCalledTimes(1);
                 done();
@@ -49,7 +57,28 @@ describe('Discord utilities', () => {
 
         it('does not fetch a thread channel by snowflake when disallowed', async (done) => {
             const fetch = spyOn(client.channels, 'fetch').withArgs('1').and.resolveTo(threadChannel);
-            getTextChannel(client, '1', false).then(() => {
+            getTextChannel(client, '1', false, false).then(() => {
+                fail();
+                done();
+            }).catch(e => {
+                expect(e.message).toBe("1 is not a text channel");
+                expect(fetch).toHaveBeenCalledTimes(1);
+                done();
+            });
+        });
+
+        it('fetches a DM channel by snowflake when allowed', async (done) => {
+            const fetch = spyOn(client.channels, 'fetch').withArgs('1').and.resolveTo(dmChannel);
+            getTextChannel(client, '1', false, true).then(chan => {
+                expect(chan).toBe(dmChannel);
+                expect(fetch).toHaveBeenCalledTimes(1);
+                done();
+            });
+        });
+
+        it('does not fetch a DM channel by snowflake when disallowed', async (done) => {
+            const fetch = spyOn(client.channels, 'fetch').withArgs('1').and.resolveTo(dmChannel);
+            getTextChannel(client, '1', false, false).then(() => {
                 fail();
                 done();
             }).catch(e => {
@@ -61,7 +90,7 @@ describe('Discord utilities', () => {
 
         it('does not fetch any other kind of channel by snowflake', async (done) => {
             const fetch = spyOn(client.channels, 'fetch').withArgs('1').and.resolveTo(otherKindOfChannel);
-            getTextChannel(client, '1', false).then(() => {
+            getTextChannel(client, '1', false, false).then(() => {
                 fail();
                 done();
             }).catch(e => {
--- src/util/discord.js
+++ src/util/discord.js
@@ -17,19 +17,23 @@
  * 
  * @param {Channel} channel Channel to test
  * @param {boolean} allowThreads Whether threads count as text channels.
+ * @param {boolean} allowDms Whether DMs count as text channels.
  * @returns {boolean}
  */
-const isTextChannel = (channel, allowThreads) => channel.isText() || (allowThreads && channel.isThread()); // TEST ME
+const isTextChannel = (channel, allowThreads, allowDms) => channel.isText()
+    && (channel.isThread() ? allowThreads : true)
+    && (channel.type === 'DM' ? allowDms : true);
 
 /**
  * 
  * @param {Client} client Discord client
  * @param {string} snowflake Channel snowflake for a text channel (or thread if allowed).
  * @param {boolean} allowThreads Whether threads should count as a text channel.
+ * @param {boolean} allowDms Whether DMs should count as a text channel.
  * @returns {Promise<TextChannel>}
  */
-const fetchTextChannel = async (client, snowflake, allowThreads) => client.channels.fetch(snowflake) // TEST ME
-    .then(ch => new Promise((resolve, reject) => isTextChannel(ch, allowThreads)
+const fetchTextChannel = async (client, snowflake, allowThreads, allowDms) => client.channels.fetch(snowflake)
+    .then(ch => new Promise((resolve, reject) => isTextChannel(ch, allowThreads, allowDms)
         ? resolve(ch)
         : reject(new Error(snowflake + " is not a text channel"))));
 
@@ -39,8 +43,9 @@ const fetchTextChannel = async (client, snowflake, allowThreads) => client.chann
  * @param {Client} client Discord client
  * @param {string|Channel} channel A channel snowflake or an existing Channel
  * @param {boolean} allowThreads Whether to allow a thread to be considered when a snowflake is provided.
+ * @param {boolean} allowDms Whether to allow a DM to be considered when a snowflake is provided.
  * @returns {Promise<TextChannel>}
  */
-export const getTextChannel = async (client, channel, allowThreads = true) => typeof channel === 'string' // TEST ME
-    ? fetchTextChannel(client, channel, allowThreads)
+export const getTextChannel = async (client, channel, allowThreads = true, allowDms = true) => typeof channel === 'string'
+    ? fetchTextChannel(client, channel, allowThreads, allowDms)
     : Promise.resolve(channel);
