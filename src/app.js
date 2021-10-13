336da680de177a689f3f602e87711d4e97515180
iyoro <6958668+iyoro@users.noreply.github.com>
2021-10-13 18:07:40 +0000

    Misc app improvements
    - Log errors to a channel
    - Re-order init too allow passing client to facade
    - Do that, so it can figure out channels itself
    - Only include DM intent if DMs enabled
    - Include CHANNEL partial to allow DMs to work
    - Log client warnings

--- src/app.js
+++ src/app.js
@@ -10,32 +10,44 @@ import ServiceAPI from './service.js';
 // Root logger
 const logger = pino({ level: config.logLevel });
 
-const facadeLogger = logger.child({ name: 'command' });
-const facade = new Facade(config, facadeLogger);
-for (let group in commandGroups) {
-    commandGroups[group](
-        facade, facadeLogger.child({ group }));
-}
-const messages = messageHandler(facade);
-
 const intents = new Discord.Intents();
 intents.add(Discord.Intents.FLAGS.GUILDS);
 intents.add(Discord.Intents.FLAGS.GUILD_MESSAGES);
-intents.add(Discord.Intents.FLAGS.DIRECT_MESSAGES);
+if (config.allowDms) {
+    intents.add(Discord.Intents.FLAGS.DIRECT_MESSAGES);
+}
+const partials = ["CHANNEL"];
+logger.debug({ intents: intents.toArray(), partials }, "Registering intents:");
 
-const client = new Discord.Client({ intents });
+const client = new Discord.Client({ intents, partials });
+
+const commandLogger = logger.child({ name: 'command' });
+const facade = new Facade(config, commandLogger, client);
+const messages = messageHandler(facade);
+
+for (let group in commandGroups) {
+    commandGroups[group](facade, commandLogger.child({ group }));
+}
+
 client.once('invalidated', () => {
     logger.info('Client invalidated, shutting down');
     process.exit(2);
 });
 client.on('rateLimit', limits => logger.info({ limits }, 'Rate limited'));
-client.on('error', err => logger.error({ err }, 'Client error'));
+client.on('error', err => {
+    logger.error({ err }, 'Client error');
+    facade.log(client, 'Client error: ' + err, false);
+});
+client.on('warn', warning => {
+    logger.warn({ warning }, 'Client warning');
+    //facade.log(client, 'Client warning: ' + warning, false);
+});
 client.on('messageCreate', messages.onMessage);
 
 const api = new ServiceAPI(client, logger);
 
 client.login(config.clientToken)
-    .then(() => logger.info('Client logged in'))
+    .then(() => facade.log(client, 'Client logged in'))
     .then(() => api.up().catch(error => {
         logger.error(error, 'API initialisation failed');
         process.exit(4);
@@ -51,7 +63,7 @@ process.on('uncaughtException', function (err) {
 });
 
 process.once('SIGINT', () => {
-    logger.info("Closing down");
+    logger.info("SIGINT - closing down");
     api.down()
         .then(() => client.destroy())
         .then(() => process.exit(0))
