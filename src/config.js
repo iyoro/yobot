1e9b544a79b695c555c2fce4ae3187deb1514315
iyoro <6958668+iyoro@users.noreply.github.com>
2021-10-13 17:55:53 +0000

    Flesh out config for new features, add comments
    - Log channel from env
    - Allow DMs/threads

--- src/config.js
+++ src/config.js
@@ -1,8 +1,21 @@
+const env = (k, def = null) => process.env[k] ? process.env[k] : def;
+
 export default {
-    logLevel: process.env.LOG_LEVEL ? process.env.LOG_LEVEL : 'debug',
-    clientToken: process.env.CLIENT_TOKEN,
-    commandPrefix: process.env.COMMAND_PREFIX ? process.env.COMMAND_PREFIX : '!',
+    // Logging verbosity
+    logLevel: env('LOG_LEVEL', 'info'),
+    // Discord API client token.
+    clientToken: env('CLIENT_TOKEN'),
+    // Magic prefix for in-chat commands (!foo)
+    commandPrefix: env('COMMAND_PREFIX', '!'),
+    // Pattern for channel names to match in order for the bot to respond there.
     channelPattern: /dice|roll|^bot/,
+    // Allow responding to DMs?
+    allowDms: true,
+    // Allow responding to threads?
+    allowThreads: true,
+    // Channel ID for a channel that certain logging can be reported to.
+    logChannel: env('LOG_CHANNEL'),
+    // Config for webservice.
     api: {
         enabled: false,
         host: 'localhost',
