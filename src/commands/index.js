a2512e2b755d77b767af6c4a9151739f47b50f81
iyoro <6958668+iyoro@users.noreply.github.com>
2021-12-27 14:10:36 +0000

    Put calendar back

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,10 +1,11 @@
 /**
  * @file Pulls together all of the command groups.
  */
+import calendar from './calendar.js';
 import def from './default.js';
 import help from './help.js';
 
 /**
  * @exports {object} Named groups of commands.
  */
-export default { /*rolls, rps,*/ help,/* calendar,*/ def, /* do not add any after def */ };
+export default { /*rolls, rps,*/ help, calendar, def, /* do not add any after def */ };
