3a96b137a7b8d0b683cdfc7206ce387f3800a093
iyoro <6958668+iyoro@users.noreply.github.com>
2021-12-27 14:08:26 +0000

    Convert to event bus

--- src/commands/calendar.js
+++ src/commands/calendar.js
@@ -2,7 +2,7 @@
  * @file Provides calendar, date and time related commands.
  */
 /** @typedef {import('../facade').default} Facade */
-/** @typedef {import('discord.js').Message } Message */
+import Events from '../events.js';
 import calendar from '../util/calendar.js';
 
 /**
@@ -17,27 +17,27 @@ export default (facade) => {
         name: 'Lore day',
         description: '`!day` Gets the current in-character lore day.',
         accept: cmd => cmd === 'day',
-        handle: async message => facade.reply(message, `It is **${calendar.day(new Date())}**`),
+        handle: async (args, context, eventBus) => eventBus.notify(Events.COMMAND_RESULT, { context, content: `It is **${calendar.day(new Date())}**` }),
     });
     facade.addCommand({
         icon: ':full_moon:',
         name: 'Lore month',
         description: '`!month` Gets the current in-character month.',
         accept: cmd => cmd === 'month',
-        handle: async message => facade.reply(message, `It is **${calendar.month(new Date())}**`),
+        handle: async (args, context, eventBus) => eventBus.notify(Events.COMMAND_RESULT, { context, content: `It is **${calendar.month(new Date())}**` }),
     });
     facade.addCommand({
         icon: ':calendar_spiral:',
         name: 'Lore date',
         description: '`!date` Gets the current in-character lore date.',
         accept: cmd => cmd === 'date',
-        handle: async message => facade.reply(message, `It is ${calendar.date(new Date())}`),
+        handle: async (args, context, eventBus) => eventBus.notify(Events.COMMAND_RESULT, { context, content: `It is ${calendar.date(new Date())}` }),
     });
     facade.addCommand({
         icon: ':last_quarter_moon:',
         name: 'Lore months',
         description: '`!months` Gets a list of lore months with the current in-character month highlighted.',
         accept: cmd => cmd === 'months',
-        handle: async message => facade.reply(message, 'Here you go!\n' + calendar.months(new Date())),
+        handle: async (args, context, eventBus) => eventBus.notify(Events.COMMAND_RESULT, { context, content: 'Here you go!\n' + calendar.months(new Date()) }),
     });
 };
